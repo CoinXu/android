5ca0bf506e1b339c2f423e995dbb7526923fb49c
Coin <duanxian0403@163.com>
2017-11-05 21:26:21 +0800

    check activity intent

--- app/src/main/java/com/example/asd/android/OpenBrowserActive.java
+++ app/src/main/java/com/example/asd/android/OpenBrowserActive.java
@@ -1,16 +1,20 @@
 package com.example.asd.android;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.util.List;
 
 public class OpenBrowserActive extends AppCompatActivity {
     private static final String LOG_TAG = "OpenBrowserActive";
 
     /**
      * open browser
+     *
      * @param savedInstanceState
      */
     @Override
@@ -23,6 +27,15 @@ public class OpenBrowserActive extends AppCompatActivity {
 
         Uri uri = Uri.parse(url);
         Intent web = new Intent(Intent.ACTION_VIEW, uri);
+
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(web, PackageManager.MATCH_ALL);
+
+        // 检测是否有可用的Activity
+        if (activities.size() == 0) {
+            return;
+        }
+
         startActivity(web);
     }
 }
